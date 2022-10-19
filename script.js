282ce7dce85e778164a85cb25179ab570fd2b8c8
tangentveil <anirudha0789@gmail.com>
2022-10-19 23:08:20 +0530

    19-Oct-2022

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navbar = document.querySelector('#navbar');
 const toggleBtn = document.querySelector('.toggle-btn');
 const closeBtn = document.querySelector('.close-btn');
+const header = document.getElementById('header');
+const topLink = document.querySelector('.top-link');
 
 
 toggleBtn.addEventListener('click', ()=>{
@@ -9,4 +11,27 @@ toggleBtn.addEventListener('click', ()=>{
 
 closeBtn.addEventListener('click', ()=>{
     navbar.classList.remove('show-navbar');
+});
+
+
+// Fixed header
+window.addEventListener('scroll', ()=>{
+    const scrollHeight = window.pageYOffset;
+    const navheight = navbar.getBoundingClientRect().height;
+
+    // console.log(navheight);
+
+    if(scrollHeight > 19){
+        header.classList.add('fixed-header');
+    } else {
+        header.classList.remove('fixed-header');
+    }
+
+
+    // back to top functionality
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
 });
